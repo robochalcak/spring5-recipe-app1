b1b6d3245522ae1f027bdf48d2ac553a9cd8f20b
Robert Chalcak <robochalcak@gmail.com>
2020-06-02 14:17:44 +0200

    Added Vet List and updated controller.Closes #15

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,4 +1,23 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.servises.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    public String ListVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
+
+        return "vets/index";
+    }
 }
